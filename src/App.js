5d59e60f46a2edfc65179e03224f592a85d8f120
SmallBulka <dolinchuk.a.20@gmail.com>
2024-04-02 16:55:26 +0500

    Intial commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,52 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header>
+        <div className='header__left'>
+          <img className='logo' src="/img/logo.svg" />
+          <div>
+            <h3>React sneakers</h3>
+            <p>Магазин лучших кроссовок</p>
+          </div>
+        </div>
+
+        <div>
+          <ul className='header__right'>
+            <li>
+            <img width={18} height={18} src="/img/Group.svg" />
+              <span>1205</span>
+            </li>
+            <li>
+            <img width={18} height={18} src="/img/zakladki.svg" />
+              <span>Закладки</span>
+            </li>
+            <li>
+            <img width={18} height={18} src="/img/Union.svg" />
+              <span>Профиль</span>
+            </li>
+          </ul>
+        </div>
       </header>
+      <div>
+        <h1>Все кроссовки</h1>
+      
+      <div className='card'>
+        <img width={133} height={112} src='/img/sneak/1.jpg'/>
+        <p>Мужские Кроссовки Nike Blazer Mid Suede</p>
+        <div className='cardFoor'>
+          <div className='cardInfo'>
+            <span>Цена:</span>
+            <b>12 999</b>
+          </div>
+          <button>
+            <img src='/img/Vector.svg'/>
+          </button>
+        </div>
+      </div>
     </div>
+    </div>
+      
   );
 }
 
