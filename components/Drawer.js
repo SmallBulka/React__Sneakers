541501ca47b46d409ae87a41043283353c10fb8f
SmallBulka <dolinchuk.a.20@gmail.com>
2024-05-06 13:27:19 +0500

    Updates

---
+++ components/Drawer.js
@@ -0,0 +1,75 @@
+function Drawer({onClose, items = [], onRemove}) {
+    return (
+      <div className="overlay">
+        <div className="drawer">
+          <h2>
+            Корзина
+            <img
+              onClick={onClose}
+              className="removBtn"
+              src="/img/btn-remov.svg"
+              alt="Remov"
+            />
+          </h2>
+          {items.length > 0 ? (
+          <div>
+            <div className="items">
+              {items.map((obj) => (
+                <div className="cartItem">
+                  <img
+                    width={70}
+                    height={70}
+                    src={obj.imageUrl}
+                    alt="Sneakers"
+                  />
+                  <div>
+                    <p>{obj.title}</p>
+                    <b>{obj.price} руб.</b>
+                  </div>
+                  <img
+                    onClick={() => onRemove(obj.id)}
+                    className="removBtn"
+                    src="/img/btn-remov.svg"
+                    alt="Remov"
+                  />
+                </div>
+              ))}
+            </div>
+            <div className="cartTotalBlock">
+            <ul>
+              <li>
+                <span>Итого: </span>
+                <div></div>
+                <b>21 498 руб. </b>
+              </li>
+              <li>
+                <span>Налог 5%: </span>
+                <div></div>
+                <b>1074 руб. </b>
+              </li>
+            </ul>
+            <button className="greenBtn">
+              Оформить заказ <img src="/img/str.svg" alt="Arrow" />
+            </button>
+          </div>
+          </div>
+          ) : (
+            <div className="close">
+              <img width="300px" height="300px" src="img/close.png" />
+              <h2>В корзине ничего нет</h2>
+              <p>
+                Выберите товары или войдите в аккаунт, если добавляли товары в
+                корзину
+              </p>
+              <button onClick={onClose} className="greenBtn">
+                <img src="/img/str.svg" alt="Arrow" /> Вернуться к покупкам
+              </button>
+            </div>
+          )}
+          {/* усли есть хоть что то рендари items ЕСЛИ нет то рендари close */}
+          
+        </div>
+      </div>
+    );
+}
+export default  Drawer;
---
+++ components/Header.js
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+function Header(props) {
+    return (
+      <header>
+        <div className="header__left">
+          
+        <Link link to="/">
+          <img src="/img/logo.svg" />
+        </Link>
+          <div>
+            <h3>React sneakers</h3>
+            <p>Магазин лучших кроссовок</p>
+          </div>
+        </div>
+
+        <div>
+          <ul className="header__right">
+            <li onClick={props.onClickCart}>
+              <img width={18} height={18} src="/img/Group.svg" />
+              <span>1205</span>
+            </li>
+            <li>
+              <Link to="/favorites">
+                <img width={18} height={18} src="/img/zakladki.svg" />
+                <span>Закладки</span>
+              </Link>
+            </li>
+            <li>
+              <img width={18} height={18} src="/img/Union.svg" />
+              <span>Профиль</span>
+            </li>
+          </ul>
+        </div>
+      </header>
+    );
+}
+export default  Header;
