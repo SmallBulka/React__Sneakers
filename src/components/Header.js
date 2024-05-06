3ddeccbfc9291382072b042d7ac1d43dec4399ee
SmallBulka <dolinchuk.a.20@gmail.com>
2024-05-06 12:35:27 +0500

    api

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
-function Header() {
+import { Link } from "react-router-dom";
+function Header(props) {
     return (
       <header>
         <div className="header__left">
+          
+        <Link link to="/">
           <img src="/img/logo.svg" />
+        </Link>
           <div>
             <h3>React sneakers</h3>
             <p>Магазин лучших кроссовок</p>
@@ -11,13 +15,15 @@ function Header() {
 
         <div>
           <ul className="header__right">
-            <li>
+            <li onClick={props.onClickCart}>
               <img width={18} height={18} src="/img/Group.svg" />
               <span>1205</span>
             </li>
             <li>
-              <img width={18} height={18} src="/img/zakladki.svg" />
-              <span>Закладки</span>
+              <Link to="/favorites">
+                <img width={18} height={18} src="/img/zakladki.svg" />
+                <span>Закладки</span>
+              </Link>
             </li>
             <li>
               <img width={18} height={18} src="/img/Union.svg" />
